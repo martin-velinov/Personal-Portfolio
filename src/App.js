739bdadc97a0d58ca7fc23c0ac2a352e02c305fe
Martin <martin.velinov24@hotmail.com>
2022-09-17 15:51:19 +0200

    initial setup of project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {Routes,Route} from 'react-router-dom'
+import Homepage from './pages/Homepage/Homepage'
+import About from './pages/About/About'
+import Projects from './pages/Projects/Projects'
+import Blog from './pages/Blog/Blog'
+import Contact from './pages/Contact/Contact'
+import Nav from './components/Nav/Nav'
+import './assets/css/Main.css'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+      <Routes>
+        <Route path='/' element={<Homepage/>}/>
+        <Route path='/about' element={<About/>}/>
+        <Route path='/projects' element={<Projects/>}/> 
+        <Route path='/blog' element={<Blog/>}/> 
+        <Route path='/contact' element={<Contact/>}/> 
+      </Routes>
     </div>
   );
 }
